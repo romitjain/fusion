220ae0444958e660b4ac4499207e48c1311cb5b1
Romit Jain <Romitjain@Romits-MacBook-Pro.local>
2016-04-18 04:56:38 +0530

    Final additions with embedded Youtube

--- twitter_feed.php
+++ twitter_feed.php
@@ -57,16 +57,17 @@
 	//print it out
 	
 	//print_r ($twitter_data[0]);
+	//print_r ($twitter_data[1]);
+
+	//print_r($twitter_data[0]->hashtags);
 
 	/*foreach ($twitter_data as $tweet) 
 	{
-		echo $tweet->text;
+		print_r($tweet->entities->hashtags[0]->text);
 		echo "<br/>";
-		echo "########";	
-		echo $tweet->text;
-		echo "@@@@@@@";
-		
 	}*/
+
+	$youtube_key = $twitter_data[0]->entities->hashtags[0]->text;
 ?>
 
 <html>
@@ -101,7 +102,95 @@
 			<div class="row background">
 				<!-- photos -->
 				<div class="col m7 s6 photos">
-						
+					<?php
+
+$htmlBody = <<<END
+<form method="GET" id="you">
+  <div>
+    <input type="hidden" id="q" name="q" placeholder="Enter Search Term" value = "ipl">
+  </div>
+  <div>
+    <input type="hidden" id="maxResults" name="maxResults" min="1" max="50" step="1" value="1">
+  </div>
+  <input type="submit" value="Search for Videos">
+</form>
+END;
+
+// This code will execute if the user entered a search query in the form
+// and submitted the form. Otherwise, the page displays the form above.
+if ($_GET['q'] && $_GET['maxResults']) {
+  // Call set_include_path() as needed to point to your client library.
+require_once 'google-api-php-client/src/Google/autoload.php';
+require_once 'google-api-php-client/src/Google/Client.php';
+require_once 'google-api-php-client/src/Google/Service/YouTube.php';
+  /*
+   * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
+   * Google Developers Console <https://console.developers.google.com/>
+   * Please ensure that you have enabled the YouTube Data API for your project.
+   */
+  $DEVELOPER_KEY = 'AIzaSyDcKa8aY_rFE_D-Lvsyn_MaQgG3Ro2CA6o';
+
+  $client = new Google_Client();
+  $client->setDeveloperKey($DEVELOPER_KEY);
+
+  // Define an object that will be used to make all API requests.
+  $youtube = new Google_Service_YouTube($client);
+
+  try {
+    // Call the search.list method to retrieve results matching the specified
+    // query term.
+    $searchResponse = $youtube->search->listSearch('id,snippet', array(
+      'q' => $_GET['q'],
+      'maxResults' => $_GET['maxResults'],
+    ));
+
+    $videos = '';
+    $channels = '';
+    $playlists = '';
+
+    // Add each result to the appropriate list, and then display the lists of
+    // matching videos, channels, and playlists.
+    foreach ($searchResponse['items'] as $searchResult) {
+      switch ($searchResult['id']['kind']) {
+        case 'youtube#video':
+          $videos .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['videoId']);
+          $embedid = $searchResult['id']['videoId'];
+          break;
+        case 'youtube#channel':
+          $channels .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['channelId']);
+          $embedid2 = $searchResult['id']['channelId'];
+          break;
+        case 'youtube#playlist':
+          $playlists .= sprintf('<li>%s (%s)</li>',
+              $searchResult['snippet']['title'], $searchResult['id']['playlistId']);
+          break;
+      }
+    }
+
+    $videolink = "http://www.youtube.com/embed/".$embedid."?autoplay=1";
+    $channellink = "http://www.youtube.com/embed/".$embedid2;
+    //echo $channellink;
+
+    $htmlBody .= <<<END
+    <h3>Videos</h3>
+    <ul>$videos</ul>
+    <iframe width="420" height="315"src=$videolink>
+    </iframe>
+END;
+  } catch (Google_Service_Exception $e) {
+    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  } catch (Google_Exception $e) {
+    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+      htmlspecialchars($e->getMessage()));
+  }
+}
+?>
+
+			<?=$htmlBody?>
+
 				</div>
 				<!-- TWEETS-->
 				<div class="col m5 s6">
